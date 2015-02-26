f0a84240c804019d4ea7810720b5f423e96e050d
Kousuke Ruichi <kou.ruichi@altplus.co.jp>
2015-02-26 18:23:19 +0900

    Add contents roughly.

--- src/Control/Concurrent/Structured.hs
+++ src/Control/Concurrent/Structured.hs
@@ -1,1 +1,13 @@
-module Control.Concurrent.Structured where
+module Control.Concurrent.Structured
+    ( module Control.Monad.Concurrent.Structured
+    , module Control.Concurrent.Structured.Base
+    , module Control.Concurrent.Structured.Exception
+    , module Control.Concurrent.Structured.STM
+    , module Control.Concurrent.Structured.Async
+    ) where
+
+import Control.Monad.Concurrent.Structured
+import Control.Concurrent.Structured.Base
+import Control.Concurrent.Structured.Exception
+import Control.Concurrent.Structured.STM
+import Control.Concurrent.Structured.Async
--- src/Control/Monad/Concurrent/Structured.hs
+++ src/Control/Monad/Concurrent/Structured.hs
@@ -1,1 +1,37 @@
 module Control.Monad.Concurrent.Structured where
+
+
+type CIO r a = ContT r IO a
+
+type CSTM r a = ContT r S.STM a
+
+type Concurrent a = CIO () a
+
+
+runCIO :: (a -> IO r) -> CIO r a -> IO r
+runCIO k action = runContT action (\a -> k a)
+{-# INLINE runCIO #-}
+
+runCSTM :: (a -> S.STM r) -> CSTM r a -> S.STM r
+runCSTM k action = runContT action (\a -> k a)
+{-# INLINE runCSTM #-}
+
+atomically :: (a -> S.STM r') -> CSTM r' a -> CIO r r'
+atomically k action = liftIO $ S.atomically $ runContT action (\a -> k a)
+{-# INLINE atomically #-}
+
+atomically_ :: CSTM r' r' -> CIO r r'
+atomically_ = atomically return
+{-# INLINE atomically_ #-}
+
+runConcurrent :: Concurrent a -> IO ()
+runConcurrent action = runContT action (const $ return ())
+{-# INLINE runConcurrent #-}
+
+runSTM :: S.STM a -> CIO r a
+runSTM m = liftIO $ S.atomically m
+{-# INLINE runSTM #-}
+
+liftSTM :: S.STM a -> CSTM r a
+liftSTM m = ContT (\k -> m >>= k)
+{-# INLINE liftSTM #-}
