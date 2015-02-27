0cfba00c2f3ceea48390cdaa3835b7d3bad33b75
Kousuke Ruichi <ruicc.rail@gmail.com>
2015-02-27 19:15:53 +0900

    Add MVar and 2 tests.

---
+++ src/Control/Concurrent/Structured/MVar.hs
@@ -0,0 +1,38 @@
+module Control.Concurrent.Structured.MVar
+    ( MV.MVar
+    , newEmptyMVar, newMVar
+    , takeMVar, putMVar, readMVar, swapMVar
+    , tryTakeMVar, tryPutMVar, isEmptyMVar
+    ) where
+
+import           Control.Monad.Concurrent.Structured (CIO, runCIO, liftIO)
+import qualified Control.Concurrent.MVar as MV
+
+type MVar = MV.MVar
+
+newEmptyMVar :: CIO r (MVar a)
+newEmptyMVar = liftIO MV.newEmptyMVar
+
+newMVar :: a -> CIO r (MVar a)
+newMVar v = liftIO $ MV.newMVar v
+
+takeMVar :: MVar a -> CIO r a
+takeMVar mv = liftIO $ MV.takeMVar mv
+
+putMVar :: MVar a -> a -> CIO r ()
+putMVar mv v = liftIO $ MV.putMVar mv v
+
+readMVar :: MVar a -> CIO r a
+readMVar mv = liftIO $ MV.readMVar mv
+
+swapMVar :: MVar a -> a -> CIO r a
+swapMVar mv v = liftIO $ MV.swapMVar mv v
+
+tryTakeMVar :: MVar a -> CIO r (Maybe a)
+tryTakeMVar mv = liftIO $ MV.tryTakeMVar mv
+
+tryPutMVar :: MVar a -> a -> CIO r Bool
+tryPutMVar mv v = liftIO $ MV.tryPutMVar mv v
+
+isEmptyMVar :: MVar a -> CIO r Bool
+isEmptyMVar mv = liftIO $ MV.isEmptyMVar mv
