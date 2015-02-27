6e7fbff73dde8e6b4d1fbe189a56ff0945512a38
Kousuke Ruichi <ruicc.rail@gmail.com>
2015-02-27 19:15:53 +0900

    Add MVar and 2 tests.

--- src/Control/Concurrent/Structured.hs
+++ src/Control/Concurrent/Structured.hs
@@ -1,6 +1,7 @@
 module Control.Concurrent.Structured
     ( module Control.Monad.Concurrent.Structured
     , module Control.Concurrent.Structured.Base
+    , module Control.Concurrent.Structured.MVar
     , module Control.Concurrent.Structured.Exception
     , module Control.Concurrent.Structured.STM
     , module Control.Concurrent.Structured.Async
@@ -8,6 +9,7 @@ module Control.Concurrent.Structured
 
 import Control.Monad.Concurrent.Structured
 import Control.Concurrent.Structured.Base
+import Control.Concurrent.Structured.MVar
 import Control.Concurrent.Structured.Exception
 import Control.Concurrent.Structured.STM
 import Control.Concurrent.Structured.Async
--- tests/Tasty.hs
+++ tests/Tasty.hs
@@ -5,10 +5,73 @@ import           Test.Tasty
 import           Test.Tasty.QuickCheck as QC
 import           System.Exit (exitFailure, exitSuccess)
 
+import           Control.Applicative
 import           Control.Concurrent.Structured
+import           Control.Exception (ErrorCall(..))
 
 
 main :: IO ()
 main = do
-    putStrLn "This test is always failed."
-    exitFailure
+    let
+        runCIO_ = runCIO return
+        tests =
+            [ runCIO_ $ do
+                tch <- newTChanCIO
+                fork_normal tch
+            , runCIO_ $ do
+                tch <- newTChanCIO
+                fork_exception tch
+            ]
+
+    b <- all id <$> sequence tests
+
+    if b
+        then exitSuccess
+        else exitFailure
+
+
+type TestChan = TChan Int
+
+fork_normal :: TestChan -> CIO r Bool
+fork_normal tch = do
+    let
+        handler _ = atomically_ $ writeTChan tch 2
+
+    notice <- newEmptyMVar
+
+    (`forkFinally_` handler) $ do
+        atomically_ $ writeTChan tch 1
+        putMVar notice ()
+
+    takeMVar notice
+
+    assertTestChan tch [1, 2]
+
+fork_exception :: TestChan -> CIO r Bool
+fork_exception tch = do
+    notice <- newEmptyMVar
+
+    let
+        handler _ = do
+            atomically_ $ writeTChan tch 2
+            tryPutMVar notice ()
+
+    (`forkFinally_` handler) $ do
+        atomically_ $ writeTChan tch 1
+        throwCIO $ ErrorCall "heyhey"
+
+    takeMVar notice
+
+    assertTestChan tch [1, 2]
+
+assertTestChan :: TestChan -> [Int] -> CIO r Bool
+assertTestChan tch [] = do
+    mx <- atomically_ $ tryReadTChan tch
+    case mx of
+        Just _ -> return False
+        Nothing -> return True
+assertTestChan tch (x:xs) = do
+    v <- atomically_ $ readTChan tch
+    if v == x
+        then assertTestChan tch xs
+        else return False
